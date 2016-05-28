ebe09520f3ffb9cee38828f3dbdd8533187507aa
Matt Higbee <mhigbee@hotmail.com>
2016-05-28 11:25:22 -0600

    saving progress

--- callbacksReview-two.js
+++ callbacksReview-two.js
@@ -37,7 +37,9 @@ var songs =
 //Above you have a large Array of Song Objects. 
 //Write a function that returns the songs array.  This function is acting like a getter.
 
-  //code here
+function getSongsArray () {
+    return songs;
+}
 
 
 //Now write a function that takes in two paramaters.  This function is acting like a setter.
@@ -45,4 +47,7 @@ var songs =
 //Inside your setter function first add the first parameter, or the song object, to the end of your songs array.
 //Once you do that, call your callback (getter) function which should get the songs and return them.
   
-  //code here
+function makeSong (song, callback) {
+  songs.push(song);
+  return callback();
+};
